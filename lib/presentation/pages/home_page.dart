7d208a0c782b1e9349281348d7a3ed3ab3638079
sciege <hamilicheslerjohn@gmail.com>
2025-12-21 03:20:56 +0800

    re-structuring home_page looks & private notes look

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -22,6 +22,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Color lockColor = Colors.grey;
   int _selectedTab = 0; // Changes when clicked
   final ScrollController _scrollController = ScrollController();
 
@@ -116,87 +117,8 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.symmetric(horizontal: 24.0),
               child: Row(
                 children: [
-                  Container(
-                    height: screenHeight * 0.3,
-                    width: screenWidth * 0.41,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF242424),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: ValueListenableBuilder<Box<hiveTodo.Todolist>>(
-                      valueListenable: Hive.box<hiveTodo.Todolist>(
-                        'todos',
-                      ).listenable(),
-                      builder: (context, box, _) {
-                        // Filter not done
-                        //TODO
-                        final domainTodos = box.keys.map((key) {
-                          final hiveTodo_ = box.get(key)!;
-                          return domainTodo.Todolist(
-                            key: key,
-                            todolist: hiveTodo_.todoList,
-                            description: hiveTodo_.description,
-                            isDone: hiveTodo_.isDone,
-                          );
-                        }).toList();
-
-                        final activeTodos = domainTodos
-                            .where((todo) => !todo.isDone)
-                            .toList();
-
-                          Widget contentWidget;
-                        if (activeTodos.isEmpty) {
-                           contentWidget =
-                          const Center(
-                            child: Text(
-                              'No active todos',
-                              style: TextStyle(
-                                color: Color(0xFFF6D14C),
-                                fontSize: 12,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          );
-                        } else {
-                          contentWidget = ListView.builder(
-                            itemCount: activeTodos.length,
-                            itemBuilder: (context, index) {
-                              final todo = activeTodos[index];
-                              return _buildtoDoCard(todo);
-                            },
-                          );
-                        }
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // THE PERMANENT TITLE
-                            TextButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => TodosPage(),
-                                  ),
-                                );
-                              },
-                              child: Text(
-                                'Todos',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 5),
-                            Expanded(child: contentWidget),
-                          ],
-                        );
-                      },
-                    ),
-                  ),
                   //_buildTodoList(screenHeight, screenWidth, hiveTodo),
+                  _todosCard(screenHeight, screenWidth),
                   const Spacer(),
                   _buildPrivate(screenHeight, screenWidth),
                 ],
@@ -254,6 +176,81 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _todosCard(screenHeight, screenWidth) {
+    return Container(
+      height: screenHeight * 0.3,
+      width: screenWidth * 0.41,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF242424),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: ValueListenableBuilder<Box<hiveTodo.Todolist>>(
+        valueListenable: Hive.box<hiveTodo.Todolist>('todos').listenable(),
+        builder: (context, box, _) {
+          // Filter not done
+          //TODO
+          final domainTodos = box.keys.map((key) {
+            final hiveTodo_ = box.get(key)!;
+            return domainTodo.Todolist(
+              key: key,
+              todolist: hiveTodo_.todoList,
+              description: hiveTodo_.description,
+              isDone: hiveTodo_.isDone,
+            );
+          }).toList();
+
+          final activeTodos = domainTodos
+              .where((todo) => !todo.isDone)
+              .toList();
+
+          Widget contentWidget;
+          if (activeTodos.isEmpty) {
+            contentWidget = const Center(
+              child: Text(
+                'No active todos',
+                style: TextStyle(color: Color(0xFFF6D14C), fontSize: 12),
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            contentWidget = ListView.builder(
+              itemCount: activeTodos.length,
+              itemBuilder: (context, index) {
+                final todo = activeTodos[index];
+                return _buildtoDoCard(todo);
+              },
+            );
+          }
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // THE PERMANENT TITLE
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TodosPage()),
+                  );
+                },
+                child: Text(
+                  'Todos',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Expanded(child: contentWidget),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
   Widget _buildPrivate(double screenHeight, double screenWidth) {
     return Container(
       height: screenHeight * 0.3,
@@ -262,6 +259,16 @@ class _HomePageState extends State<HomePage> {
       decoration: BoxDecoration(
         color: const Color(0xFF242424),
         borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: GestureDetector(
+          child: IconButton(
+            color: lockColor,
+            onPressed: () {},
+            iconSize: 30,
+            icon: Icon(Icons.lock_outline),
+          ),
+        ),
       ),
     );
   }
@@ -381,22 +388,35 @@ class _HomePageState extends State<HomePage> {
 
             const Spacer(),
             Container(
-              padding: const EdgeInsets.all(12),
+              padding: const EdgeInsets.all(5),
               decoration: const BoxDecoration(
                 color: Color(0xFFF6D14C),
                 shape: BoxShape.circle,
               ),
-              child: const Icon(Icons.add, color: Colors.black, size: 28),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.edit_outlined,
+                  color: Colors.black,
+                  size: 25,
+                ),
+
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NotesPage()),
+                  );
+                },
+              ),
             ),
 
             const Spacer(),
-
             IconButton(
-              icon: const Icon(Icons.edit_outlined, color: Colors.white),
+              icon: const Icon(Icons.lock_outline, color: Colors.white),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const NotesPage()),
+                  //todo set up
+                  MaterialPageRoute(builder: (context) => TodolistPage()),
                 );
               },
             ),
