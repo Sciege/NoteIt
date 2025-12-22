e7ee1c22bbb5ac3a0d6e097d0d0daff91aff9808
sciege <hamilicheslerjohn@gmail.com>
2025-12-22 23:33:27 +0800

    fixed bugs

--- android/app/src/main/kotlin/com/example/notes_it/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/notes_it/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.notes_it
 
 import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity()
+//import io.flutter.embedding.android.FlutterActivity
+class MainActivity : FlutterFragmentActivity() {}
